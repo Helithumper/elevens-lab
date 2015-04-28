61d0fab1b7cef85dbd6520bb6c0c8f5e6e911efd
Peyton Duncan <peyton.duncan45@gmail.com>
2015-04-27 20:52:09 -0400

    Activity One Done

--- Activity Starter Code/Activity1 Starter Code/CardTester.java
+++ Activity Starter Code/Activity1 Starter Code/CardTester.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * This is a class that tests the Card class.
  */
@@ -8,6 +10,20 @@ public class CardTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		ArrayList<Card> cards = new ArrayList<Card>();
+		cards.add(new Card("Eight","Spades",8));
+		cards.add(new Card("Nine","Hearts",9));
+		cards.add(new Card("Eight","Spades",8));
+		
+		Card matchTest = new Card("Nine","Hearts",9);
+		
+		System.out.println("TESTING!");
+		for(Card c:cards){
+			System.out.println("###########################\n"+c.toString());
+			System.out.println("Rank: " + c.rank());
+			System.out.println("Suit: " + c.suit());
+			System.out.println("Value: " + c.pointValue());
+			System.out.println("Is Same As Test Card? " + c.matches(matchTest));
+		}
 	}
 }
